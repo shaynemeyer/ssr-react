20d517c9d9ad5d2807cd0b6f02eb5505f4b5275a
Shayne Meyer <shayne.meyer@gmail.com>
2017-11-14 07:39:13 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Server Side Render Boilerplate
+This is a boilerplate for making Server Side Rendered React apps.
+
---
+++ ssr-boilerplate/webpack.server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  // Inform webpack that we're building a bundle for nodejs
+  target: "node",
+
+  // Tell webpack the root of our server app.
+  entry: './src/index.js',
+
+  // Tell webpack where to put the output file
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'build')
+  },
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: {browsers: ['last 2 versions']}}]
+          ]
+        }
+      }
+    ]
+  }
+}
