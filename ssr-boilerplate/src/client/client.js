be1b59893eb87c147264e120babe86658045dc2c
Shayne Meyer <shayne.meyer@gmail.com>
2017-11-15 08:05:59 -0800

    added reducers

--- ssr-boilerplate/src/client/client.js
+++ ssr-boilerplate/src/client/client.js
@@ -1,6 +1,21 @@
 // Startup point for client side app
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import Routes from './Routes';
+import reducers from './reducers';
 
-ReactDOM.hydrate(<Home/>, document.querySelector("#root"));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
+);
--- ssr-boilerplate/webpack.server.js
+++ ssr-boilerplate/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack that we're building a bundle for nodejs
@@ -13,7 +14,9 @@ const config = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
